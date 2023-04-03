3f82d13ff1d3db409d93b12b8231571261995d58
maxsvst <fioklin2015@mail.ru>
2023-04-03 17:48:51 +0300

    Добавление всех необходимых маршрутов

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ const Cors = require("cors");
 const BodyParser = require("body-parser");
 
 const { dataBase } = require(__dir.libs + "/dataBase");
-const apiRouter = require(__dir.routers + "/api");
+
+const teacherRouter = require(__dir.routers + "/teacher");
+const competenceRouter = require(__dir.routers + "/competence");
+const disciplineRouter = require(__dir.routers + "/discipline");
+const examQuestionsRouter = require(__dir.routers + "/examQuestions");
+const laboratoryClassRouter = require(__dir.routers + "/laboratoryClass");
+const lectionsRouter = require(__dir.routers + "/lections");
+const practicalClassRouter = require(__dir.routers + "/practicalClass");
+const rpdRouter = require(__dir.routers + "/rpd");
+const topicRouter = require(__dir.routers + "/topic");
 
 const app = Express();
 const PORT = process.env.PORT || 8080;
@@ -20,7 +29,15 @@ app.use(BodyParser.urlencoded({ extended: true }));
 app.use(Cors());
 app.use(Express.json());
 
-app.use("/api", apiRouter);
+app.use("/teacher", teacherRouter);
+app.use("/competence", competenceRouter);
+app.use("/discipline", disciplineRouter);
+app.use("/examQuestions", examQuestionsRouter);
+app.use("/laboratoryClass", laboratoryClassRouter);
+app.use("/lections", lectionsRouter);
+app.use("/practicalClass", practicalClassRouter);
+app.use("/rpd", rpdRouter);
+app.use("/topic", topicRouter);
 
 const start = async () => {
   try {
